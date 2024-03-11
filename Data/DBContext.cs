c35740b6af2d3ffc9dca91675731b513ed739612
Monoclocker <52466843+Monoclocker@users.noreply.github.com>
2024-03-11 21:54:45 +0300

    Добавьте файлы проекта.

---
+++ Data/DBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using UniversitySystem.Models;
+
+namespace UniversitySystem.Data
+{
+    public class DBContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UniversitySystem.Models
+{
+    public class User: IdentityUser<int>
+    {
+    }
+}
